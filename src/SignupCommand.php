f67aefb44300c56cb05112facfef985459082f0d
Nilambar Sharma <nilambar@outlook.com>
2024-03-13 16:27:51 +0545

    Add delete subcommand

--- src/SignupCommand.php
+++ src/SignupCommand.php
@@ -3,6 +3,7 @@
 namespace WP_CLI\Signup;
 
 use WP_CLI;
+use WP_CLI\Utils;
 use WP_CLI_Command;
 
 class SignupCommand extends WP_CLI_Command {
@@ -28,6 +29,9 @@ class SignupCommand extends WP_CLI_Command {
 	 * [--field=<field>]
 	 * : Prints the value of a single field for each signup.
 	 *
+	 * [--<field>=<value>]
+	 * : Filter results by key=value pairs.
+	 *
 	 * [--fields=<fields>]
 	 * : Limit the output to specific object fields.
 	 *
@@ -38,8 +42,10 @@ class SignupCommand extends WP_CLI_Command {
 	 * options:
 	 *   - table
 	 *   - csv
+	 *   - ids
 	 *   - json
 	 *   - yaml
+	 *   - count
 	 * ---
 	 *
 	 * ## AVAILABLE FIELDS
@@ -77,14 +83,41 @@ class SignupCommand extends WP_CLI_Command {
 			$assoc_args['fields'] = $this->obj_fields;
 		}
 
-		$results = $wpdb->get_results( "SELECT * FROM $wpdb->signups WHERE active = 0" );
+		$signups = array();
 
-		$formatter = new WP_CLI\Formatter( $assoc_args );
-		$formatter->display_items( $results );
+		$results = $wpdb->get_results( "SELECT * FROM $wpdb->signups", ARRAY_A );
+
+		if ( $results ) {
+			foreach ( $results as $r ) {
+				$item = $r;
+				unset( $item['meta'] );
+
+				// Support features like --active=0.
+				foreach ( array_keys( $r ) as $field ) {
+					if ( isset( $assoc_args[ $field ] ) && $assoc_args[ $field ] !== $r[ $field ] ) {
+						continue 2;
+					}
+				}
+
+				$signups[] = $item;
+			}
+		}
+
+		$format = Utils\get_flag_value( $assoc_args, 'format', 'table' );
+
+		if ( 'count' === $format ) {
+			WP_CLI::line( count( $signups ) );
+		} elseif ( 'ids' === $format ) {
+			$formatter = new WP_CLI\Formatter( $assoc_args );
+			$formatter->display_items( wp_list_pluck( $signups, 'signup_id' ) );
+		} else {
+			$formatter = new WP_CLI\Formatter( $assoc_args, $this->obj_fields );
+			$formatter->display_items( $signups );
+		}
 	}
 
 	/**
-	 * Activate a user.
+	 * Activate an user.
 	 *
 	 * ## OPTIONS
 	 *
@@ -112,6 +145,40 @@ class SignupCommand extends WP_CLI_Command {
 			WP_CLI::success( "User activated. Password: {$result['password']}" );
 		} else {
 			WP_CLI::error( 'User could not be activated. Reason: ' . $result->get_error_message() );
+		}
+	}
+
+	/**
+	 * Delete an user from signups.
+	 *
+	 * ## OPTIONS
+	 *
+	 * <user>
+	 * : ID, user login, user email or activation key.
+	 *
+	 * ## EXAMPLES
+	 *
+	 *     # Delete an user from signups.
+	 *     $ wp signup delete johndoe@example.com
+	 *     Success: User deleted.
+	 */
+	function delete( $args, $assoc_args ) {
+		global $wpdb;
+
+		$signup = $this->fetcher->get( $args[0] );
+
+		if ( false === $signup ) {
+			WP_CLI::error( 'Signup not found.' );
+		}
+
+		if ( $signup ) {
+			$wpdb->delete( $wpdb->signups, array( 'signup_id' => $signup->signup_id ), array( '%d' ) );
+		}
+
+		if ( $wpdb->rows_affected > 0 ) {
+			WP_CLI::success( 'User deleted.' );
+		} else {
+			WP_CLI::error( 'Error occurred while deleting user.' );
 		}
 	}
 }
