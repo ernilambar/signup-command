a02104ea66336284ec3ae8d42ea7876ab0900a45
Nilambar Sharma <nilambar@outlook.com>
2024-03-13 14:52:13 +0545

    Implement listing

--- src/SignupCommand.php
+++ src/SignupCommand.php
@@ -7,13 +7,73 @@ use WP_CLI_Command;
 
 class SignupCommand extends WP_CLI_Command {
 
+	protected $obj_fields = array(
+		'signup_id',
+		'user_login',
+		'user_email',
+		'registered',
+		'active',
+		'activation_key',
+	);
+
 	/**
-	 * List signups.
+	 * Lists signups.
 	 *
-	 * @param array $args       Indexed array of positional arguments.
-	 * @param array $assoc_args Associative array of associative arguments.
+	 * [--field=<field>]
+	 * : Prints the value of a single field for each signup.
+	 *
+	 * [--fields=<fields>]
+	 * : Limit the output to specific object fields.
+	 *
+	 * [--format=<format>]
+	 * : Render output in a particular format.
+	 * ---
+	 * default: table
+	 * options:
+	 *   - table
+	 *   - csv
+	 *   - json
+	 *   - yaml
+	 * ---
+	 *
+	 * ## AVAILABLE FIELDS
+	 *
+	 * These fields will be displayed by default for each signup:
+	 *
+	 * * signup_id
+	 * * user_login
+	 * * user_email
+	 * * registered
+	 * * active
+	 * * activation_key
+	 *
+	 * These fields are optionally available:
+	 *
+	 * * domain
+	 * * path
+	 * * title
+	 * * activated
+	 *
+	 * ## EXAMPLES
+	 *
+	 *     # List signup IDs.
+	 *     $ wp signup list --field=signup_id
+	 *     1
+	 *
+	 * @package wp-cli
 	 */
 	public function list( $args, $assoc_args ) {
-		WP_CLI::success( 'Hello World!' );
+		global $wpdb;
+
+		if ( isset( $assoc_args['fields'] ) ) {
+			$assoc_args['fields'] = explode( ',', $assoc_args['fields'] );
+		} else {
+			$assoc_args['fields'] = $this->obj_fields;
+		}
+
+		$results = $wpdb->get_results( "SELECT * FROM $wpdb->signups WHERE active = 0" );
+
+		$formatter = new WP_CLI\Formatter( $assoc_args );
+		$formatter->display_items( $results );
 	}
 }
